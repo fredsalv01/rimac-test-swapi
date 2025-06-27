d4b89b229f46a9a7b4d99d58b284cb5c81d246af
fredsalv01 <freddy.morales@usil.pe>
2025-06-27 00:48:47 -0500

    fix: try deploy of serverless first lambda function

--- src/handler.ts
+++ src/handler.ts
@@ -4,14 +4,17 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
+  console.log("GET / called");
   res.status(200).json({ message: "Hello from root!" });
 });
 
 app.get("/hello", (req: Request, res: Response) => {
+  console.log("GET /hello called");
   res.status(200).json({ message: "Hello from path!" });
 });
 
 app.use((req: Request, res: Response) => {
+  console.log("NOT FOUND / called");
   res.status(404).json({ error: "Not Found" });
 });
 
